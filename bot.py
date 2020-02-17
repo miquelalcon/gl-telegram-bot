de258860a34b80e04104b927ab561d09713fd3b5
miquelalcon <miquel.1996@gmail.com>
2020-02-17 10:53:13 +0100

    debug

--- bot.py
+++ bot.py
@@ -38,6 +38,7 @@ def lunch_time():
 @app.route('/', methods=['POST'])
 def main():
     data = request.json
+    print(data)
 
     # print(data)  # Comment to hide what Telegram is sending you
     if 'message' in data and 'text' in data['message']:
