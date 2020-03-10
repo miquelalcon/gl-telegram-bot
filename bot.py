cad62cf5960192e445c44379e0d80d0ae7579fae
miquelalcon <miquel.1996@gmail.com>
2020-03-10 09:58:41 +0100

    query test

--- bot.py
+++ bot.py
@@ -280,7 +280,7 @@ def create_app():
     scheduler.start()
     init_striked()
     db_init()
-    db_insert('strikes', {'chat_id': lunch_chat_id, 'user': os.environ["STRIKED"]})
+    #db_insert('strikes', {'chat_id': lunch_chat_id, 'user': os.environ["STRIKED"]})
     print(db_query('strikes', {'chat_id': lunch_chat_id}))
     cursor.execute("SELECT * FROM strikes")
     print(cursor.fetchall())
