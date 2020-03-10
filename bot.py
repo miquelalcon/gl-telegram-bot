e3ddcc7792fcea3fc4888a93c202c6627ca8fa84
miquelalcon <miquel.1996@gmail.com>
2020-03-10 10:37:05 +0100

    del unused fetchall

--- bot.py
+++ bot.py
@@ -281,7 +281,6 @@ def create_app():
     scheduler.start()
     init_striked()
     db_init()
-    print(cursor.fetchall())
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
 
