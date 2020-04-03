f47e9a01ec9f05eccafb8d6304dedd9e61a33fe7
miquelalcon <miquel.1996@gmail.com>
2020-04-03 14:13:19 +0200

    add markdown

--- bot.py
+++ bot.py
@@ -310,7 +310,7 @@ def main():
                     db_update(table_name, data)
                 
                 if data['count']%10 == 0:
-                    send_message(chat_id, 'Nuestro querido @%s acumula un total **%d** Fs. ¡Una F por él!'%(message_usr, data['count']))
+                    send_message(chat_id, 'Nuestro querido @%s acumula un total *%d* Fs. ¡Una F por él!'%(message_usr, data['count']), parse_mode='Markdown')
 
     if 'poll' in data and data['poll']['is_closed']:
         options = data['poll']['options']
